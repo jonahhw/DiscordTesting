e25e76a7c2b3899329b51d7037bf006efb93b4f6
jonahhw <jonahhw@gmail.com>
2020-07-01 11:45:57 -0400

    Add ability to recognize messages Now raises an exception and handles it when a certain message is sent, and responds to another message

--- bot.py
+++ bot.py
@@ -12,6 +12,31 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to discord!')
+    print(f'{client.user} is connected to the following guilds:')
+    for guild in client.guilds:
+        print(f'  {guild.name} (id: {guild.id}), which has these members:')
+        members = ', '.join([member.name for member in guild.members])
+        print(f'    {members}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    print(f'{str(message.author)}: {message.content}')
+    if message.content == "test":
+        await message.channel.send("You sent a test")
+    elif message.content == "raise-exception":
+        raise discord.DiscordException
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open("err.log", "a+") as f:
+        if event == "on_message":
+            f.write(f"unhandled message written by {args[0].author.name} at {args[0].created_at}\n")
+            f.write(f"  Full data: {args[0]}\n")
+        else:
+            raise
+
 
 client.run(TOKEN)
