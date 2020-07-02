0acfb111bcce88af3f158736647c528b523ac584
jonahhw <jonahhw@gmail.com>
2020-07-02 01:56:03 -0400

    Add list function, make it work for custom emoji as well

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 import discord
 
 TOKEN = env.DISCORD_TOKEN       # The token, taken from env.py (which is hidden from the git repo)
+NOT_ACTIVE_MESSAGE = "RoleBot is not active in {channel_name}"
 
 bot = commands.Bot(command_prefix="rb ")
 
@@ -16,6 +17,20 @@ EmojiAssignments = {}   # A dictionary of dictionaries. EmojiAssignments[Channel
 async def on_ready():
     guilds = ", ".join(guild.name for guild in bot.guilds)
     print(f"{bot.user} is ready and connected to {guilds}")
+
+@bot.command(name="list", help=" - Lists the emoji and their corresponding roles")
+async def list_emoji(ctx):
+    if ctx.channel not in EmojiAssignments:
+        await ctx.send(NOT_ACTIVE_MESSAGE.format(channel_name = ctx.channel.name))
+        return
+    message_text = "Current bindings:"
+    for emoji in EmojiAssignments[ctx.channel]:
+        message_text += f"\n{emoji}: {EmojiAssignments[ctx.channel][emoji].name}"
+    await ctx.send(message_text)
+    return
+    message = await ctx.send(message_text)
+    for emoji in EmojiAssignments[ctx.channel]:
+        await message.add_reaction(emoji)
 
 @bot.command(name="activate", help=" - Activates the bot in the channel in which it's run.")
 async def activate(ctx):        # If it's not already there, it adds a dictionary under the channel in which this is run
@@ -37,7 +52,7 @@ async def deactivate(ctx):      # Removes the dectionary entry for this channel
 @bot.command(name="assign", help=" - Assigns an emoji to a role. Usage: `rb assign [emoji] [role name]`")
 async def assign_emoji(ctx, emoji, roleString):
     if ctx.channel not in EmojiAssignments:
-        await ctx.send(f"RoleBot is not active in {ctx.channel.name}")
+        await ctx.send(NOT_ACTIVE_MESSAGE.format(channel_name = ctx.channel.name))
         return
     role = discord.utils.find(lambda r: r.name == roleString, ctx.guild.roles)
     if not role:
@@ -48,14 +63,14 @@ async def assign_emoji(ctx, emoji, roleString):
         PreviousRole = EmojiAssignments[ctx.channel][emoji]
     EmojiAssignments[ctx.channel][emoji] = role
     if PreviousRole:
-        await ctx.send(f"Reassigning {emoji} from {PreviousRole.name} to {EmojiAssignments[ctx.channel][emoji].name}")
+        await ctx.send(f"Reassigned {emoji} from {PreviousRole.name} to {EmojiAssignments[ctx.channel][emoji].name}")
     else:
         await ctx.send(f"{emoji} asssigned to {EmojiAssignments[ctx.channel][emoji].name}")
 
 @bot.command(name="deassign", help=" - Removes the association from an emoji to a role. Usage: `rb deassign [emoji]`")
 async def deassign_emoji(ctx, emoji):
     if ctx.channel not in EmojiAssignments:
-        await ctx.send(f"RoleBot is not active in {ctx.channel.name}")
+        await ctx.send(NOT_ACTIVE_MESSAGE.format(channel_name = ctx.channel.name))
         return
     PreviousRole = None
     if emoji in EmojiAssignments[ctx.channel]:
@@ -69,21 +84,21 @@ async def deassign_emoji(ctx, emoji):
 @bot.event
 async def on_reaction_add(reaction, user):
     cnl = reaction.message.channel
-    if (cnl not in EmojiAssignments) or (reaction.message.author != bot.user) or (reaction.emoji not in EmojiAssignments[cnl]):
+    if (cnl not in EmojiAssignments) or (reaction.message.author != bot.user) or (str(reaction.emoji) not in EmojiAssignments[cnl]):
         return
-    await user.add_roles(EmojiAssignments[cnl][reaction.emoji], reason="RoleBot")
-    print(f"{user.name} given role {EmojiAssignments[cnl][reaction.emoji].name}")
+    await user.add_roles(EmojiAssignments[cnl][str(reaction.emoji)], reason="RoleBot")
+    print(f"{user.name} given role {EmojiAssignments[cnl][str(reaction.emoji)].name}")
 
 @bot.event
 async def on_reaction_remove(reaction, user):
     cnl = reaction.message.channel
-    if (cnl not in EmojiAssignments) or (reaction.message.author != bot.user) or (reaction.emoji not in EmojiAssignments[cnl]):
+    if (cnl not in EmojiAssignments) or (reaction.message.author != bot.user) or (str(reaction.emoji) not in EmojiAssignments[cnl]):
         return
-    if EmojiAssignments[cnl][reaction.emoji] not in user.roles:
-        print(f"{user.name} did not have role {EmojiAssignments[cnl][reaction.emoji].name}")
+    if EmojiAssignments[cnl][str(reaction.emoji)] not in user.roles:
+        print(f"{user.name} did not have role {EmojiAssignments[cnl][str(reaction.emoji)].name}")
         return
-    await user.remove_roles(EmojiAssignments[cnl][reaction.emoji], reason="RoleBot")
-    print(f"{user.name} lost role {EmojiAssignments[cnl][reaction.emoji].name}")
+    await user.remove_roles(EmojiAssignments[cnl][str(reaction.emoji)], reason="RoleBot")
+    print(f"{user.name} lost role {EmojiAssignments[cnl][str(reaction.emoji)].name}")
 
 @bot.event
 async def on_error(event, *args, **kwargs):
